7a4a9b8b3f382fb26c63d479bb346be0f2240a15
Alex <alexs123655@gmail.com>
2021-11-02 22:37:32 -0300

    chore: setup project

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { 
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
+import { Home } from '../pages/home';
+
+export function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
