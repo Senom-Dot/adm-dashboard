eea5f92fb751a9d71646abcd700cc29a6ed99f03
Alex <alexs123655@gmail.com>
2021-11-04 09:16:34 -0300

    chore: setup global styles

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -6,4 +6,15 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         box-sizing: border-box;
     }
+
+    body {
+        background: var(--background);
+    }
+
+    :root {
+        --background: #0e0824;
+        --title-color: #2E384D;
+        --button-color: #2e384d;
+        --white: #ffffff;
+    }
 `;
