c47fbadb2c4d3428758c51686d0e6158f1a6c2c2
Alex <alexs123655@gmail.com>
2021-11-04 09:21:57 -0300

    feat: create auth page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,71 @@
 import React from "react";
 
+import {
+    Container,
+    Main,
+    Form,
+    Input,
+} from "./style";
+
+import { useAuth } from '../../hooks/useAuth';
+
+import { Button } from './../../components/Button/index';
+import { Title } from "../../components/Title";
+
+import { BiHide } from 'react-icons/bi';
+
 export function Home() {
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const [showPass, setShowPass] = React.useState(false);
+
+    const { signIn } = useAuth();
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        if(!username.trim() || !password){
+            return;
+        }
+
+        await signIn({ username, password })
+    };
+
+    function handleShow() {
+        return setShowPass(!showPass);
+    };
+
     return (
-        <>
-            <h1>Hello world</h1>
-        </>
+        <Container>
+            <Main>
+                <Title text="Attackz"/>
+
+                <Form onSubmit={handleSubmit}>
+                    <label>
+                        <span>Username</span>
+                        <Input
+                            placeholder="Username.."
+                            onChange={event => setUsername(event.target.value)}
+                            required    
+                        />
+                    </label>
+
+                    <label>
+                        <span>Password</span>
+                        <Input
+                            placeholder="Password.."
+                            onChange={event => setPassword(event.target.value)}
+                            type={!showPass ? 'password' : 'text'}
+                            required    
+                        />
+
+                        <i className="form__hideIcon" onClick={handleShow}><BiHide /></i>
+                    </label>
+
+                    <Button text="SignIn" onClick={handleSubmit} />
+                </Form>
+            </Main>
+        </Container>
     )
 }
