ee09e0d1e26c0e909c432fee179a8a90affe4a06
Alex <alexs123655@gmail.com>
2021-11-11 07:40:12 -0300

    chore: add global custom fonts

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
+import Roboto from '../assets/fonts/Roboto/Roboto-Regular.ttf';
+import RobotoMedium from '../assets/fonts/Roboto/Roboto-Regular.ttf';
+
 export const GlobalStyle = createGlobalStyle`
     * {
         padding: 0;
@@ -7,14 +10,27 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
 
+    
     body {
         background: var(--background);
     }
-
+    
     :root {
         --background: #0e0824;
         --title-color: #2E384D;
         --button-color: #2e384d;
         --white: #ffffff;
+    }
+
+    // Global Custom Fonts
+    
+    @font-face {
+        font-family: 'Roboto';
+        src: url(${Roboto}) format('truetype');
+    }
+
+    @font-face {
+        font-family: 'RobotoMedium';
+        src: url(${RobotoMedium}) format('truetype');
     }
 `;
