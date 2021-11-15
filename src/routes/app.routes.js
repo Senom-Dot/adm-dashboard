07c8fba7a67a68f5d49c63185f508ed61085288f
Alex <alexs123655@gmail.com>
2021-11-14 22:38:54 -0300

    feat: add new pages

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -9,13 +9,17 @@ import {
 import { NewUser } from '../pages/createUser';
 import { Dashboard } from '../pages/dashboard/index';
 import { NotFound } from '../pages/error';
+import { Settings } from '../pages/settings';
+import { Users } from '../pages/users';
 
 export function App() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" component={Dashboard} exact/>
+                <Route path="/users" component={Users}/>
                 <Route path="/new-user" component={NewUser}/>
+                <Route path="/settings" component={Settings}/>
                 <Route component={NotFound}/>
             </Switch>
         </BrowserRouter>
