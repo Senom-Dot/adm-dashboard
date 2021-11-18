b42fafff59f90d75a8ad14069a3c2616f52abcd7
Al3xsandro <alexs123655@gmail.com>
2021-11-17 21:26:19 -0300

    chore: create dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Container, Grid, Card } from "./style";
+
 import { Link } from "react-router-dom";
 
 import { Header } from "../../components/Header";
-import { Container } from "./style";
+import { SideBar } from "../../components/SideBar";
+
+import { FiUsers } from 'react-icons/fi';
+import { GoSettings } from 'react-icons/go';
+
+import { HashLoader } from 'react-spinners';
 
 export function Dashboard() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 1000);
+
     return (
         <>
             <Header/>
 
+            <SideBar/>
+            
             <Container>
-                <Link to="/new-user">Create new user</Link>
+                {
+                    isLoading ? (
+                        <div className="loading">
+                            <HashLoader color="ABB69F" size="100px"/>
+                        </div>
+                    ) : (
+                        <>
+                             <div className="container__title">
+                                <h2 className="title"><strong>Dashboard</strong></h2>
+                            </div>
+                            
+                            <Grid>
+                                <Card>       
+                                    <Link to="/users">
+                                        <FiUsers size="50" color="#ffff"/>
+                                        Users
+                                    </Link>
+                                </Card>
+
+                                <Card style={{ background: '#136897'}}>       
+                                    <Link to="/settings">
+                                        <GoSettings size="50" color="#ffff"/>
+                                        Settings
+                                    </Link>
+                                </Card>
+                            </Grid>
+                        </>
+                    )
+                }
             </Container>
         </>
     )
--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -1,10 +1,95 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    margin-left: 18rem;
+    padding-top: 5rem;
+
+    background: #eeee;
+    
+    .container__title {
+        padding: 2rem;
+        
+        background: #ffffff6b;
+
+        border-bottom: 1px solid #2d29292b;
+        
+        h2 {
+            font-size: 1.2rem;
+            font-family: RobotoMedium, sans-serif;
+            font-weight: 500;
+            color: var(--title-color);
+        }
+
+        strong {
+            color: #49bb25;
+        }
+    }
+
+    .loading {
+        display: flex;
+        flex-direction: column;
+
+        align-items: center;
+    }
+
+    @media(max-width: 768px){
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        margin-left: 0;
+        padding-top: 0;
+    }
+`;
+
+export const Grid = styled.section`
+    height: 100%;
+
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+
+    margin: 2rem;
+
+    gap: 1rem;
+
+    @media(max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+    }
+`;
+
+export const Card = styled.div`
+    height: 8rem;
+    width: 8rem;
+
     display: flex;
     justify-content: center;
     flex-direction: column;
     align-items: center;
 
-    margin: 1rem;
+    padding: 1.5rem;
+
+    border-radius: 1.2rem;
+
+    background: #00b360;
+
+    a {
+        text-align: center;
+        font-family: Roboto, sans-serif;
+        font-size: 1rem;
+        color: #fff;
+        text-decoration: none;
+
+        cursor: pointer;
+    }
+
+    &:hover {
+        filter: brightness(0.9);
+    }
 `;
